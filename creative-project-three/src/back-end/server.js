9920efecd0a3faa92cffb3d82a1e249ed27924d2
Eian <wrighteian@gmail.com>
2022-11-10 16:22:21 -0700

    Set up what I think I'll need for a backend.

---
+++ creative-project-three/src/back-end/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+let savedMemes = [];
+
+app.post('/api/memes/user/add/:username', (req, res) => {
+    let username = req.params.username;
+    let requestedUserMemes = savedMemes.find(memes => memes.username == username);
+    if (requestedUserMemes === undefined) {
+        requestedUserMemes = {
+            username: username,
+            savedMemes: []
+        };
+        savedMemes.push(requestedUserMemes);
+    }
+    res.send(requestedUserMemes);
+});
+
+app.post('/api/memes/meme/add/:username', (req, res) => {
+    let username = req.params.username;
+    let requestedUserMemes = savedMemes.find(memes => memes.username == username);
+    if (requestedUserMemes === undefined) {
+        res.status(404)
+            .send("That user does not exist.");
+        return;
+    }
+    else {
+        requestedUserMemes.savedMemes.push(req.body);
+        res.send(req.body);
+        return;
+    }
+});
+
+app.get('/api/memes/meme/getall/:username', (req, res) => {
+    let username = req.params.username;
+    let requestedUserMemes = savedMemes.find(memes => memes.username == username);
+    if (requestedUserMemes === undefined) {
+        res.status(404)
+            .send("That user does not exist.");
+        return;
+    }
+    else {
+        res.send(requestedUserMemes.savedMemes);
+        return;
+    }
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
