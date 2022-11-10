20dbdb7954087ef5004c6eda6388cd6fcc90799a
Eian <wrighteian@gmail.com>
2022-11-10 08:23:42 -0700

    Got the url to call the API with key attached. Got it to be called on page load and whenever new meme is requested.

--- creative-project-three/src/App.js
+++ creative-project-three/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import Authentication from './Authentication';
+let authentication = new Authentication();
 
 function App() {
+  const [getMeme, setGetMeme] = useState(false);
+  const dataFetchedRef = useRef(false);
+
+  const getRandomMeme = async () => {
+    try {
+      let url = authentication.appendAPIKEY("https://api.humorapi.com/memes/random?media-type=image&api-key=");
+      console.log(url);
+    }
+    catch (error) {
+      console.log("Error");
+    }
+  }
+
+  // This and the dataFetchedRef are only needed in development to avoid calling the API twice on page reload.
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    getRandomMeme();
+  })
+
+  useEffect(() => {
+    if (getMeme) {
+      getRandomMeme()
+      setGetMeme(false);
+    }
+  }, [getMeme]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        setGetMeme(true);
+        }}>Get Random Meme</button>
     </div>
   );
 }
